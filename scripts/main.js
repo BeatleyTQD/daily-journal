6b506598ec0e297512a904b81ad855401ac13130
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-18 13:34:22 -0500

    started breaking big js code block into modularized pieces

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,15 @@
 import API from "./data.js"
 import journalMaker from "./entryList.js"
 import createJournalEntry from "./createEntry.js"
+// import journalGrabber from "./journalGrabber.js"  --think this is the direction to go
 
-// API.getJournalEntries()
-// .then((res)=> {
-//     journalMaker.renderJournalEntries(res)
-// })
+
 
 document.querySelector("#submit").addEventListener("click", clickEvent => {
     const date = document.querySelector("#journalDate").value
-    const concept = document.querySelector("#conceptsCovered").value
+    const concept = document.querySelector("#conceptCovered").value
     const content = document.querySelector("#journalEntryContent").value
     const mood = document.querySelector("#mood").value
-
     //checks to see that all fields are filled, if not alerts user to complete it and resubmit
     if (date !== "" && concept !== "" && content !== "" && mood !== "") {
         let newEntry = createJournalEntry(date, concept, content, mood)
